37af3f3f2ea76f6b993d79bebde74ab8f186d913
gaopu_dxm <gaopu@duxiaoman.com>
2018-10-27 18:52:55 +0800

    readme

--- CrawlPandaDanmu/readme.md
+++ CrawlPandaDanmu/readme.md
@@ -1,3 +1,3 @@
-PandaTV弹幕抓取工具
+PandaTV弹幕抓取工具，具体说明见[知乎](https://www.zhihu.com/question/38807641/answer/84007935)
 
-直接运行main就行
+[www.panda.tv](https://www.panda.tv/) barrage crawler, run with Start.java.
--- CrawlPandaDanmu/src/main/java/Start.java
+++ CrawlPandaDanmu/src/main/java/Start.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 /**
  * Created by geekgao on 16-1-29.
  */
-public class Main {
+public class Start {
     public static void main(String[] args) throws IOException {
         Crawl c = new Crawl();
         c.start();
